30301f7d51403ee71041dc4e48483ac2bce3c0e6
DSK <42357923+talveth@users.noreply.github.com>
2025-05-12 09:34:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.sendFile("frontpage.html"));
-
+app.get('/', (req, res) => res.sendFile("frontpage.html", {root: __dirname}));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
